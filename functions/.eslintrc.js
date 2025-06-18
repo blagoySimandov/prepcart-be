97bcd6993138e295db1814b563300e5003cb401a
blagoySimandov <baggy.simandoff@gmail.com>
2025-06-18 10:32:58 +0300

    eslint:

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -16,13 +16,17 @@ module.exports = {
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
+    tsconfigRootDir: __dirname,
   },
   ignorePatterns: ["/lib/**/*", "/generated/**/*", ".eslintrc.js"],
   plugins: ["@typescript-eslint", "import"],
   rules: {
+    "quote-props": 0,
+    indent: 0,
     quotes: ["error", "double"],
     "require-jsdoc": 0,
     "import/no-unresolved": 0,
+    "operator-linebreak": 0,
     indent: ["error", 2],
     "object-curly-spacing": ["error", "always"],
     "max-len": [
